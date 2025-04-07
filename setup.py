9263c3416ccb5c2e4f58034f9bca4089b33b88c8
Amirhossein Foroughi <amirhosseinforoughi2@gmail.com>
2025-04-07 12:41:28 -0600

    Fix build workflow and update dependencies

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version="1.0.6",
     packages=find_packages(),
     install_requires=[
-        'pandas==2.1.0',
+        'pandas==2.0.3',
         'openpyxl==3.1.2',
         'watchdog==4.0.0',
         'PySide6==6.6.1',
