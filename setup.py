24cc4f5c8479339d4cd95eb534c2c42037f1325a
Amirhossein Foroughi <amirhosseinforoughi2@gmail.com>
2025-04-07 12:37:27 -0600

    Update dependencies and version to 1.0.1

--- setup.py
+++ setup.py
@@ -2,14 +2,15 @@ from setuptools import setup, find_packages
 
 setup(
     name="ExcelMonitor",
-    version="1.0.0",
+    version="1.0.1",
     packages=find_packages(),
     install_requires=[
-        'pandas',
-        'openpyxl',
-        'watchdog',
+        'pandas==2.1.0',
+        'openpyxl==3.1.2',
+        'watchdog==4.0.0',
         'PySide6==6.6.1',
-        'requests'
+        'requests==2.31.0',
+        'python-dotenv==1.0.1'
     ],
     author="ExcelMonitor",
     description="An application to monitor Excel files and sync changes to cloud",
